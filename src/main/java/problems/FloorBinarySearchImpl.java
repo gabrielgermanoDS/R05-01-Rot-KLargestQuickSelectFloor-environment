9db8240fa8a4c05cc9a28972fccf2f8e2cc36de3
GbrlGrmno <103541716+GbrlGrmno@users.noreply.github.com>
2024-08-13 16:50:22 -0300

    feat: Implementação Floor Binary Search

--- src/main/java/problems/FloorBinarySearchImpl.java
+++ src/main/java/problems/FloorBinarySearchImpl.java
@@ -4,8 +4,39 @@ public class FloorBinarySearchImpl implements Floor {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		return binarySearch(array, 0, array.length - 1, x);
+		
+	}
+
+	public Integer binarySearch(Integer[] array, int l, int r, Integer x) {
+		if (x < array[0]) {
+            return null;
+        }
+
+		int m;
+
+		while (l <= r) {
+
+			m = (l + r) / 2; 
+
+			if (array[m].equals(x)) {
+				return array[m];
+			}
+
+			if (l == r) {
+				return array[m];
+			}
+
+			else if (x.compareTo(array[m]) > 0) {
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+
+		}
+
+		return null;
 	}
 
 }
